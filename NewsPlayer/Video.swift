346259475fcec019870c3ea9d97cc0f7a72b6eb6
ymkjp <lq33xxx@gmail.com>
2015-09-25 19:50:52 +0900

    Add state to Video class

--- NewsPlayer/Video.swift
+++ NewsPlayer/Video.swift
@@ -14,17 +14,24 @@ class Video: Equatable {
         var width: Int
         var height: Int
     }
+    enum State {
+        case Waiting
+        case Watching
+        case Watched
+    }
 
     var id: String
     var title: String
     var description: String
     var thumbnail: Thumbnail
+    var state: State
 
     init(id: String, title: String, description: String, thumbnail: Thumbnail) {
         self.id = id
         self.title = title
         self.description = description
         self.thumbnail = thumbnail
+        self.state = State.Waiting
     }
 }
 
