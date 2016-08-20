1e82dc857b5119070591cb81703f54b565d717b6
Kenta Yamamoto <ymkjp@users.noreply.github.com>
2016-08-20 18:26:30 +0900

    Get ready for testing

---
+++ NewsPlayerTests/ChannelModelTest.swift
@@ -0,0 +1,27 @@
+//
+//  ChannelModelTest.swift
+//  NewsPlayer
+//
+//  Created by YAMAMOTOKenta on 8/20/16.
+//  Copyright © 2016 ymkjp. All rights reserved.
+//
+
+import XCTest
+
+class ChannelModelTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+    
+    }
+    
+}
